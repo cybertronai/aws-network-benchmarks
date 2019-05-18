07741e2a8e9f0cf819188f4ca8f097e7c17e4299
Yaroslav Bulatov <yaroslavvb@gmail.com>
2019-05-17 20:44:43 -0700

    AWS benchmark that tests different nccl versions

---
+++ nccl_multiversion.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# python nccl_multiversion.py
+
+import argparse
+import os
+
+import ncluster
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', type=str, default='nccl_multiversion')
+parser.add_argument('--instance_type', type=str, default="p3.8xlarge")
+parser.add_argument('--num_tasks', type=int, default=2, help="number of nodes")
+parser.add_argument('--spot', action='store_true', help='use spot instances')
+parser.add_argument('--skip_setup', action='store_true',
+                    help='can use this option on reruns for slightly faster turn-around')
+parser.add_argument('--image_name', type=str, default='Deep Learning AMI (Ubuntu) Version 22.0')
+
+
+# TODO(y) (not needed?) pytorch 1.0.1/2.3.7+cuda10.0
+parser.add_argument('--conda_env', type=str, default='pytorch_p36')
+parser.add_argument('--role', type=str, default='launcher')
+parser.add_argument('--nproc_per_node', type=int, default=0, help='number of processes to launch, if not specified, set automatically from number of gpus on instance')
+
+args = parser.parse_args()
+
+
+def launcher():
+    os.environ['NCLUSTER_AWS_FAST_ROOTDISK']='1' # request disk with lots of IOPS on AWS
+    job = ncluster.make_job(**vars(args))
+
+    job.rsync('.')
+
+    if not args.nproc_per_node:
+        args.nproc_per_node = job.tasks[0].num_gpus
+
+    def nccl_build(tag, gitcmd):
+        job.run(f'export NCCL_VERSION_TAG="{tag}"')
+        job.run(f'export GIT_CHECKOUT_CMD="{gitcmd}"')
+        job.run(f'source ~/parameterized_nccl_build.sh')
+
+    MPI_HOME='/usr/local/mpi'  # for DLAMI 22
+    job.run(f"export MPI_HOME={MPI_HOME}")
+    job.run("export NCCL_SOCKET_IFNAME=ens5") # TODO(y): remove because p3dn specific
+
+    should_nccl_build = True
+    
+    if should_nccl_build:
+        nccl_build('2.3.7', "git checkout v2.3.7-1")
+        nccl_build('2.4.7', "git checkout v2.4.7-1")
+        nccl_build('2.4.7ms0', "git checkout dev/kwen/multi-socket")
+
+    # setup password-less SSH between all pairs of instances
+    public_keys = {}
+    for task in job.tasks:
+        key_fn = '~/.ssh/id_rsa'  # this fn is special, used by default by ssh
+        task.run(f"yes | ssh-keygen -t rsa -f {key_fn} -N ''")
+
+        public_keys[task] = task.read(key_fn + '.pub')
+
+    for task1 in job.tasks:
+        task1.run('echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config',
+                  sudo=True, non_blocking=True)
+        for task2 in job.tasks:
+            # task1 ->ssh-> task2
+            task2.run(f'echo "{public_keys[task1]}" >> ~/.ssh/authorized_keys',
+                      non_blocking=True)
+
+    # launch MPI
+    hosts = [task.ip for task in job.tasks]
+    host_str = ','.join(hosts)
+
+    task0 = job.tasks[0]
+
+    # sanity check, simple mpirun that will print hostnames
+    task0.run(f'{MPI_HOME}/bin/mpirun --host {host_str} hostname')
+
+    np = args.nproc_per_node * args.num_tasks
+    tag = '2.3.7'
+    task0.run(f'{MPI_HOME}/bin/mpirun --host {host_str} -np {np} -N {args.nproc_per_node} '
+              f'-x LD_LIBRARY_PATH=~/nccl/nccl-{tag}/nccl/build/lib:$LD_LIBRARY_PATH '
+              f'~/nccl/nccl-{tag}/nccl-tests/build/all_reduce_perf -b 8 -e 128M -f 2 -g {args.nproc_per_node}')
+
+
+def main():
+    if args.role == "launcher":
+        launcher()
+    elif args.role == "worker":
+        assert False, 'unknown arg'
+    else:
+        assert False, "Unknown role " + args.role
+
+
+if __name__ == '__main__':
+    main()
---
+++ parameterized_nccl_build.sh
@@ -0,0 +1,95 @@
+# Builds nccl and nccl-tests for specific nccl version.
+# Paths specialized to DLAMI 22, cuda-10
+#
+# Parameters:
+# MPI_HOME:  (must be set for the nccl-tests to build, ie MPI_HOME=/usr/local/mpi)
+# NCCL_VERSION_TAG: for folder name, git clone called from
+#     ~/nccl/nccl-$NCCL_VERSION_TAG
+# GIT_CHECKOUT_COMMAND: command called after git clone, right before building
+# NCCL_WIPE_PREVIOUS_BUILD: if set, will get rid of previous build artifacts
+#
+# Result:
+#  ~/nccl/nccl-$NCCL_VERSION_TAG/nccl/build/lib/libnccl.so
+#  ~/nccl/nccl-$NCCL_VERSION_TAG/nccl-tests/build/build/all_reduce_perf
+#
+# Examples:
+# MPI_HOME=/usr/local/mpi
+# NCCL_VERSION_TAG=2.3.7
+# GIT_CHECKOUT_CMD="git checkout v2.3.7-1"
+# source parameterized_nccl_build.sh
+#
+# MPI_HOME=/usr/local/mpi
+# NCCL_VERSION_TAG=2.4.7
+# GIT_CHECKOUT_CMD="git checkout v2.4.7-1"
+# source parameterized_nccl_build.sh
+#
+# MPI_HOME=/usr/local/mpi
+# NCCL_VERSION_TAG=2.4.7ms0
+# GIT_CHECKOUT_CMD="git checkout dev/kwen/multi-socket"
+# source parameterized_nccl_build.sh
+
+pushd .
+
+# change default cuda to cuda-10, possibly not needed
+sudo rm /usr/local/cuda
+sudo ln -s /usr/local/cuda-10.0 /usr/local/cuda
+
+if [ -z ${NCCL_VERSION_TAG+x} ]; then
+    NCCL_VERSION_TAG="default"
+    echo "Using default tag $NCCL_VERSION_TAG"
+else
+    echo "Using existing tag $NCCL_VERSION_TAG"
+fi
+
+if [ -z ${GIT_CHECKOUT_CMD+x} ]; then
+    GIT_CHECKOUT_CMD="skipping checkout"
+    echo "Using default git checkout cmd $GIT_CHECKOUT_CMD"
+else
+    echo "Using existing git checkout cmd $GIT_CHECKOUT_CMD"
+fi
+
+mkdir -p ~/nccl/nccl-$NCCL_VERSION_TAG
+cd ~/nccl/nccl-$NCCL_VERSION_TAG
+git clone https://github.com/NVIDIA/nccl.git || echo "exists"
+
+cd nccl
+$GIT_CHECKOUT_CMD
+if [ -z ${NCCL_WIPE_PREVIOUS_BUILD+x} ]; then
+    echo "NCCL_WIPE_PREVIOUS_BUILD not set, reusing previous build"
+else
+    echo "NCCL_WIPE_PREVIOUS_BUILD is set, building from scratch"
+    rm -Rf build
+fi
+
+make -j src.build
+
+
+# Library goes to ~/nccl/nccl-$NCCL_VERSION_TAG/nccl/build/lib/libnccl.so
+# .so is taken from LD_LIBRARY_PATH by default
+export LD_LIBRARY_PATH=~/nccl/nccl-$NCCL_VERSION_TAG/nccl/build/lib:$LD_LIBRARY_PATH
+# PyTorch install asks for NCCL_ROOT_DIR
+export NCCL_ROOT_DIR=~/nccl/nccl-$NCCL_VERSION_TAG/nccl
+# NCCL_HOME is used for linking of nccl-tests
+export NCCL_HOME=~/nccl/nccl-$NCCL_VERSION_TAG/nccl/build
+
+# Build nccl examples
+cd ~/nccl/nccl-$NCCL_VERSION_TAG
+git clone https://github.com/NVIDIA/nccl-tests.git || echo "exists"
+
+cd ~/nccl/nccl-$NCCL_VERSION_TAG/nccl-tests
+if [ -z ${NCCL_WIPE_PREVIOUS_BUILD+x} ]; then
+    echo "NCCL_WIPE_PREVIOUS_BUILD not set, reusing previous examples build"
+else
+    echo "NCCL_WIPE_PREVIOUS_BUILD is set, building from scratch"
+    make clean
+fi
+
+if [ -z ${MPI_HOME+x} ]; then
+    echo "Warning, MPI_HOME is not set"
+else
+    echo "Using MPI_HOME=$MPI_HOME"
+fi
+
+make MPI=1 CUDA_HOME=/usr/local/cuda-10.0
+
+popd
