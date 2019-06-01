4c0cf40b5a3d53ab4ee819d334b4b1ae3ab38145
Yaroslav Bulatov <yaroslavvb@gmail.com>
2019-06-01 15:11:24 -0700

    clean-up

--- nccl_multiversion.py
+++ nccl_multiversion.py
@@ -19,14 +19,16 @@ parser.add_argument('--force_rebuild', type=int, default=0)
 parser.add_argument('--do_efa', type=int, default=1)
 parser.add_argument('--do_efa_hack', type=int, default=0)
 parser.add_argument('--role', type=str, default='launcher')
-parser.add_argument('--nproc_per_node', type=int, default=0, help='number of processes to launch, if not specified, set automatically from number of gpus on instance')
-parser.add_argument('--num_gpus', type=int, default=0, help='number of processes to launch, if not specified, set automatically from number of gpus on instance')
+parser.add_argument('--nproc_per_node', type=int, default=0,
+                    help='number of processes to launch, if not specified, set automatically from number of gpus on instance')
+parser.add_argument('--num_gpus', type=int, default=0,
+                    help='number of processes to launch, if not specified, set automatically from number of gpus on instance')
 
 args = parser.parse_args()
 
 
 def launcher():
-    #    os.environ['NCLUSTER_AWS_FAST_ROOTDISK'] = '1' # request disk with lots of IOPS on AWS
+    os.environ['NCLUSTER_AWS_FAST_ROOTDISK'] = '1'  # request disk with lots of IOPS on AWS
     job = ncluster.make_job(**vars(args))
 
     if not args.nproc_per_node:
@@ -42,9 +44,6 @@ def launcher():
             job.run(f'export NCCL_VERSION_TAG="{nccl_version_tag}"')
             job.run(f'export GIT_CHECKOUT_CMD="{gitcmd}"')
             job.run(f'source ~/parameterized_nccl_build.sh')
-
-            #        job.run(f"export MPI_HOME={MPI_HOME}")
-            #        job.run("export NCCL_SOCKET_IFNAME=ens5") # TODO(y): remove because p3dn specific
 
         # nccl_build('2.3.7', "git checkout v2.3.7-1")
         # nccl_build('2.4.7ms0', "git checkout dev/kwen/multi-socket")
@@ -70,78 +69,59 @@ def launcher():
     else:
         print(f"{setup_completed_fn} found, skipping setup")
 
-
-    
     # launch MPI
     hosts = [task.ip for task in job.tasks]
     host_str = ','.join(hosts)
 
     task0 = job.tasks[0]
 
-    
     #    nccl_version_tag = '2.4.7'
     #    nccl_version_tag = '2.3.7'
     #    nccl_version_tag = '2.4.7ms0'
     NCCL_VERSION_TAG = '2.4.6'
     FOLDER_ROOT = f"{task0.homedir}/nccl/nccl-{NCCL_VERSION_TAG}"
-    CUDA_HOME=f'/usr/local/cuda-10.0'
-    MPI_HOME=f'{task0.homedir}/anaconda3'
-    NCCL_HOME=f'{FOLDER_ROOT}/nccl/build'
-    EFA_HOME=f'/opt/amazon/efa'
+    CUDA_HOME = f'/usr/local/cuda-10.0'
+    MPI_HOME = f'{task0.homedir}/anaconda3'
+    NCCL_HOME = f'{FOLDER_ROOT}/nccl/build'
+    EFA_HOME = f'/opt/amazon/efa'
 
     # sanity check, simple mpirun that will print hostnames
     task0.run(f'{MPI_HOME}/bin/mpirun --host {host_str} hostname')
 
-
-    np = args.nproc_per_node
-
     # https://github.com/NVIDIA/nccl-tests/issues/21
     cmd = (f'{MPI_HOME}/bin/mpirun --host {host_str} -np {args.num_gpus} '
            f'-N 8 '
-           f'-mca btl ^openib ' # get rid of no infiniband warning '
-           f'-mca oob_tcp_if_include ens5 -mca btl_tcp_if_include ens5 ' # force ens5
-           f'-mca orte_base_help_aggregate 0 '   # more logging messages
+           f'-mca btl ^openib '  # get rid of no infiniband warning '
+           f'-mca oob_tcp_if_include ens5 -mca btl_tcp_if_include ens5 '  # force ens5
+           f'-mca orte_base_help_aggregate 0 '  # more logging messages
            f'-x LD_LIBRARY_PATH={NCCL_HOME}/lib:$LD_LIBRARY_PATH '
-           f'-oversubscribe ' # for "There are not enough slots" error
+           f'-oversubscribe '  # for "There are not enough slots" error
            f'{FOLDER_ROOT}/nccl-tests/build/all_reduce_perf -b 1280M -e 1280M -f 2 ')
 
     cmd_efa = (f'{MPI_HOME}/bin/mpirun '
-               f'-x FI_PROVIDER="efa" '
-               f'-x FI_OFI_RXR_RX_COPY_UNEXP=1 -x FI_OFI_RXR_RX_COPY_OOO=1 '
-               f'-x FI_EFA_MR_CACHE_ENABLE=1 -x FI_OFI_RXR_INLINE_MR_ENABLE=1 '
-               f'-x LD_LIBRARY_PATH={FOLDER_ROOT}/aws-ofi-nccl/install/lib/:{NCCL_HOME}/lib:{CUDA_HOME}/lib64:{EFA_HOME}/lib64:{MPI_HOME}/lib:$LD_LIBRARY_PATH '
-               f'-x NCCL_DEBUG=INFO -x NCCL_TREE_THRESHOLD=0 --host localhost -n 2 -N 2 '
-               f'--mca btl tcp,self --mca btl_tcp_if_exclude lo,docker0 --bind-to none '
-               f'--oversubscribe '
+               f'-x FI_PROVIDER="efa" '  # Enables running nccl-tests using EFA provider.
+               f'-x FI_OFI_RXR_RX_COPY_UNEXP=1 '  #  Disables using bounce buffers for unexpected messages.
+               f'-x FI_OFI_RXR_RX_COPY_OOO=1 '  # Disables using bounce buffers for out of order messages.
+               f'-x FI_EFA_MR_CACHE_ENABLE=1 '  # Enables memory region caching.
+               f'-x FI_OFI_RXR_INLINE_MR_ENABLE=1 '  # Enables inline memory registration of data buffers.
+               f'-x LD_LIBRARY_PATH='
+               f'{FOLDER_ROOT}/aws-ofi-nccl/install/lib/:'
+               f'{NCCL_HOME}/lib:'
+               f'{CUDA_HOME}/lib64:'
+               f'{EFA_HOME}/lib64:'
+               f'{MPI_HOME}/lib:$LD_LIBRARY_PATH '
+               f'-x NCCL_DEBUG=INFO '  # print NCCL version info
+               f'-x NCCL_TREE_THRESHOLD=0 '  # Disable tree-algorithm, faster for <8 instances
+               f'--host localhost -n 2 -N 2 '
+               f'--mca btl tcp,self --mca btl_tcp_if_exclude lo,docker0 '
+               f'--bind-to none '
+               f'--oversubscribe '  # # https://github.com/NVIDIA/nccl-tests/issues/21
                f'{FOLDER_ROOT}/nccl-tests/build/all_reduce_perf -b 8 -e 1G -f 2 -g 1 -c 1 -n 2')
 
-    
-    # cmd_efa = (f'$HOME/anaconda3/bin/mpirun '
-    #            f'-x FI_PROVIDER="efa" '
-    #            f'-x FI_OFI_RXR_RX_COPY_UNEXP=1 '
-    #            f'-x FI_OFI_RXR_RX_COPY_OOO=1 '
-    #            f'-x FI_EFA_MR_CACHE_ENABLE=1 '
-    #            f'-x FI_OFI_RXR_INLINE_MR_ENABLE=1 '
-    #            f'-x LD_LIBRARY_PATH={ofi_path}:{nccl_path}:/usr/local/cuda-10.0/lib64:/opt/amazon/efa/lib64:$LD_LIBRARY_PATH '
-    #            f'-x NCCL_DEBUG=INFO -x '
-    #            f'NCCL_TREE_THRESHOLD=0 '
-    #            f'--host {host_str} '
-    #            f'-n 16 '
-    #            f'-N 8 '
-    #            f'-oversubscribe ' # for "There are not enough slots" error
-    #            '--mca btl tcp,self '
-    #            f'--mca btl_tcp_if_exclude lo,docker0 '
-    #            f'--bind-to none '
-    #            f'~/nccl/nccl-{nccl_version_tag}/nccl-tests/build/all_reduce_perf -b 8 -e 1G -f 2 -g 1 -c 1 -n 16')
-
-    # https://github.com/NVIDIA/nccl-tests/issues/21
     if args.do_efa:
         cmd = cmd_efa
-    elif args.do_efa_hack:
-        cmd = cmd_efa_hack
-    assert not (args.do_efa and args.do_efa_hack)
-    
-    task0.run(cmd)    
+
+    task0.run(cmd)
 
     print(task0.output)
 
